f0a2ad3592d015568bd6fd42deaac7d06f08e615
RuzilKhafizov <68954480+ParZIvalXL@users.noreply.github.com>
2025-02-04 19:42:33 +0300

    Update

--- BombersMans/Assets/Scripts/GameController.cs
+++ BombersMans/Assets/Scripts/GameController.cs
@@ -8,11 +8,22 @@ public class GameController : MonoBehaviour
     public Queue<Action> actions = new Queue<Action>();
     [SerializeField] public Tilemap ActionTilemap;
     public static GameController Instance;
+    private string[,] tiles;
+    public Grid grid;
+
+    private string[,] ReadMap()
+    {
+        tiles = new string[20, 20];
+        var childCount = grid.transform.childCount;
+
+        return tiles;
+    }
     
     public void AddAction(Action action)
     {
         actions.Enqueue(action);
     }
+    
     void Awake()
     {
         Instance = this;
