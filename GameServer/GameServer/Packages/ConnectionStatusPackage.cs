32f1c8d3a7e430014c81d6fea52b827b93c759d3
timurfajzullin <144335476+timurfajzullin@users.noreply.github.com>
2025-02-05 20:54:29 +0300

    update

---
+++ GameServer/GameServer/Packages/ConnectionStatusPackage.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Packages;
+
+public class ConnectionStatusPackage
+{
+    public int ConnectionState { get; set; } // - статус подключения
+    public string ConnectionDescription { get; set; } // - описание подключения
+
+    public string Type {get; set;} = "ConnectionStatusPackage";
+}
